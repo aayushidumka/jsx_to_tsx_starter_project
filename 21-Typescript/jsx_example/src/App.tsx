2ff2c995c2b6cc6dce229a576d8d9c2141350c38
aayushidumka <aayushi.dumka@gmail.com>
2025-04-03 22:26:42 -0500

    resolving errors

--- 21-Typescript/jsx_example/src/App.tsx
+++ 21-Typescript/jsx_example/src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '../public/vite.svg'
 import './App.css'
 import SumComponent from './components/SumComponent'; // Import the SumComponent
-
+import React from 'react';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0)
 
   return (
     <>
